62d69abfe0c4e0e7c605d5290035771fb40f2128
imrul18 <imrul.afnan18@gmail.com>
2023-10-15 23:16:10 +0600

    update instructions

--- src/Component/Layout/MainLayout.js
+++ src/Component/Layout/MainLayout.js
@@ -58,6 +58,7 @@ const MainLayout = () => {
           onClick={({ key }) => {
             if (key === "signout") {
             } else {
+              handleToggle()
               navigate(key);
             }
           }}
